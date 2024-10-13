afe9075525935a21377ca9a43fce1b8168850e9a
schumaker <hudson.schumaker@me.com>
2024-10-13 14:13:20 +0200

    Force-app: add table schema.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,16 @@ CREATE TABLE IF NOT EXISTS product (
     description VARCHAR(255),
     price DOUBLE NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS tickets (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    ticket_id CHAR(36) NOT NULL,
+    first_name VARCHAR(64) NOT NULL,
+    last_name VARCHAR(64) NOT NULL,
+    email VARCHAR(128) NOT NULL,
+    country VARCHAR(2) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    INDEX idx_email (email),
+    INDEX idx_ticket_id (ticket_id)
+);
