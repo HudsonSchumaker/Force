26ef6ca74619cf3dd55f2df981d52905ec8c8e82
Hudson Schumaker <hudson.schumaker@me.com>
2025-05-13 20:09:32 +0200

    Force: Add MockHttpExchange for tests.

---
+++ src/test/java/br/com/schumaker/force/framework/web/http/MockHttpExchange.java
@@ -0,0 +1,113 @@
+package br.com.schumaker.force.framework.web.http;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpPrincipal;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.URI;
+
+public class MockHttpExchange extends HttpExchange {
+    private Headers requestHeaders = new Headers();
+    private Headers responseHeaders = new Headers();
+    private InputStream requestBody;
+
+    public MockHttpExchange() {}
+
+    public void setRequestHeaders(Headers headers) {
+        this.requestHeaders.clear();
+        this.requestHeaders = headers;
+        var x = 0;
+    }
+
+    @Override
+    public Headers getRequestHeaders() {
+        return requestHeaders;
+    }
+
+    public void setResponseHeaders(Headers headers) {
+        this.responseHeaders.clear();
+        this.responseHeaders = headers;
+    }
+
+    @Override
+    public Headers getResponseHeaders() {
+        return responseHeaders;
+    }
+
+    @Override
+    public URI getRequestURI() {
+        return null;
+    }
+
+    @Override
+    public String getRequestMethod() {
+        return "";
+    }
+
+    @Override
+    public HttpContext getHttpContext() {
+        return null;
+    }
+
+    @Override
+    public void close() {}
+
+    public void setRequestBody(InputStream requestBody) {
+        this.requestBody = requestBody;
+    }
+
+    @Override
+    public InputStream getRequestBody() {
+        return requestBody;
+    }
+
+    @Override
+    public OutputStream getResponseBody() {
+        return null;
+    }
+
+    @Override
+    public void sendResponseHeaders(int rCode, long responseLength) throws IOException {
+    }
+
+    @Override
+    public InetSocketAddress getRemoteAddress() {
+        return null;
+    }
+
+    @Override
+    public int getResponseCode() {
+        return 0;
+    }
+
+    @Override
+    public InetSocketAddress getLocalAddress() {
+        return null;
+    }
+
+    @Override
+    public String getProtocol() {
+        return "";
+    }
+
+    @Override
+    public Object getAttribute(String name) {
+        return null;
+    }
+
+    @Override
+    public void setAttribute(String name, Object value) {}
+
+    @Override
+    public void setStreams(InputStream i, OutputStream o) {}
+
+    @Override
+    public HttpPrincipal getPrincipal() {
+        return null;
+    }
+}
