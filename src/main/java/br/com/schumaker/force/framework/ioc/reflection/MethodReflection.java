9ea79d7ce0911a1e8224f4de23009253cc74e3c0
Hudson Schumaker <hudson.schumaker@me.com>
2025-05-27 14:32:31 +0200

    Force: update method reflection.

--- src/main/java/br/com/schumaker/force/framework/ioc/reflection/MethodReflection.java
+++ src/main/java/br/com/schumaker/force/framework/ioc/reflection/MethodReflection.java
@@ -1,9 +1,13 @@
 package br.com.schumaker.force.framework.ioc.reflection;
 
+import br.com.schumaker.force.framework.ioc.annotations.Scheduled;
 import br.com.schumaker.force.framework.ioc.annotations.bean.Bean;
 import br.com.schumaker.force.framework.exception.ForceException;
 import br.com.schumaker.force.framework.ioc.IoCContainer;
 import br.com.schumaker.force.framework.ioc.managed.ManagedBean;
+import br.com.schumaker.force.framework.scheduler.Scheduler;
+
+import java.lang.reflect.Modifier;
 
 /**
  * The MethodReflection class provides utility methods for handling methods annotated with @Bean.
@@ -11,10 +15,11 @@ import br.com.schumaker.force.framework.ioc.managed.ManagedBean;
  * This class is a singleton and provides a global point of access to its instance.
  *
  * @author Hudson Schumaker
- * @version 1.0.0
+ * @version 1.1.0
  */
 public final class MethodReflection {
     private static final MethodReflection INSTANCE = new MethodReflection();
+    private final Scheduler scheduler = Scheduler.getInstance();
 
     private MethodReflection() {}
 
@@ -36,6 +41,42 @@ public final class MethodReflection {
                     Object bean = method.invoke(instance);
                     var managedBean = ManagedBean.builder(bean.getClass(), bean);
                     IoCContainer.getInstance().registerBean(managedBean);
+                }
+            }
+        } catch (Exception ex) {
+            throw new ForceException(ex.getMessage());
+        }
+    }
+
+    public void scheduleTask(Object instance) {
+        try {
+            var methods = instance.getClass().getDeclaredMethods();
+            for (var method : methods) {
+                if (Modifier.isPublic(method.getModifiers())) {
+                    if (method.isAnnotationPresent(Scheduled.class)) {
+                        var annotation = method.getAnnotation(Scheduled.class);
+                        var initialDelay = annotation.initialDelay();
+                        var period = annotation.period();
+                        var unit = annotation.unit();
+
+                        if (initialDelay < 0 || period <= 0) {
+                            throw new ForceException("Invalid initial delay or period for scheduled method: " + method.getName());
+                        }
+
+                        Runnable task = () -> {
+                            try {
+                                method.invoke(instance);
+                            } catch (Exception ex) {
+                                throw new ForceException(ex.getMessage());
+                            }
+                        };
+
+                        if (initialDelay > 0) {
+                            scheduler.scheduleAtFixedRate(task, initialDelay, period, unit);
+                        } else {
+                            scheduler.schedule(task, period, unit);
+                        }
+                    }
                 }
             }
         } catch (Exception ex) {
